1fbb54d1ba43c9b225ca2ca124b2814ac42e4c10
madebyirfan <momin.irfan2024@gmail.com>
2025-08-07 23:17:32 +0530

    first commit

--- liveblocks.config.ts
+++ liveblocks.config.ts
@@ -3,16 +3,16 @@ import { createClient } from "@liveblocks/client";
 import { createRoomContext } from "@liveblocks/react";
 
 // Use the custom auth endpoint
-const client = createClient({
-  authEndpoint: "/api/liveblocks-auth", // This calls your route.ts file
-});
+// const client = createClient({
+//   authEndpoint: "/api/liveblocks-auth",
+// });
 
 // public key is not needed for server-side operations, but you can still use it if you want to access public data
 // Uncomment the following lines if you need to use the public key in your client-side code
 
-// const client = createClient({
-//   publicApiKey: process.env.NEXT_PUBLIC_LIVEBLOCKS_PUBLIC_KEY!,
-// });
+const client = createClient({
+  publicApiKey: process.env.NEXT_PUBLIC_LIVEBLOCKS_PUBLIC_KEY!,
+});
 
 export const {
   RoomProvider,
